d4c8da67925db3e49ab49e78bf266c8fe28f0ce4
Rajesh-Saini-dav <rajeshsaini7042@gmail.com>
2026-02-12 19:52:31 +0530

    update

--- src/Components/Tabbing.jsx
+++ src/Components/Tabbing.jsx
@@ -1,25 +1,5 @@
 import { useState } from "react";
-
-const tabsData = [
-  {
-    label: "Faq",
-    content:
-      "Here is some faq inquiries like.What is your return and exchange policy?",
-  },
-  {
-    label: "Overview",
-    content: "This is the overview tab content. It gives a quick summary.",
-  },
-  {
-    label: "Features",
-    content: "This tab lists all the features of the product or service.",
-  },
-  {
-    label: "Pricing",
-    content:
-      "Here you can see pricing details and subscription plans.&pricing details of the any product",
-  },
-];
+import { tabsData } from "./Data";
 
 function Tabs() {
   const [activeTab, setActiveTab] = useState(0);
