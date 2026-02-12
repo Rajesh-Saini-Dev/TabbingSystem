a8daa729b9e3d17a2b477d3a3c5753964b4d5471
Rajesh-Saini-dav <rajeshsaini7042@gmail.com>
2026-02-12 19:51:38 +0530

    date move to different file

---
+++ src/Components/Data.jsx
@@ -0,0 +1,22 @@
+export const tabsData = [
+  {
+    label: "Faq",
+    content:
+      "Here is some faq inquiries like.What is your return and exchange policy?  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta esse ipsa obcaecati nobis quasi reiciendis ipsum tempore, adipisci dignissimos officiis!",
+  },
+  {
+    label: "Overview",
+    content:
+      "This is the overview tab content. It gives a quick summary. lorem30 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti perferendis dolorum non voluptatem, praesentium a, nihil reprehenderit ipsam quod atque quia quas repellendus nisi, rem nesciunt iusto est doloribus libero quo? Labore inventore exercitationem nam, esse voluptas odit excepturi necessitatibus.",
+  },
+  {
+    label: "Features",
+    content:
+      "This tab lists all the features of the product or service.Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, pariatur neque. Natus ipsam, harum nihil et dicta labore ad, temporibus quasi consequuntur, molestias porro sit pariatur odit. Itaque, quisquam. Sapiente?",
+  },
+  {
+    label: "Pricing",
+    content:
+      "Here you can see pricing details and subscription plans.&pricing details of the any product Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis, illum esse facilis quos non quam dolor maiores explicabo molestias tempore itaque ducimus alias asperiores ipsa.",
+  },
+];
